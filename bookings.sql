7eb62c049e9379e349dc2681cae6fc4efe44bbe4
manoj <manoj.dulani@gmail.com>
2018-11-10 19:44:41 -0600

    first commit

---
+++ bookings.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS project_database.bookings (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(20) null,
+email VARCHAR(20) null,
+passengers VARCHAR(2) null,
+departure date null
+)ENGINE=innodb;
